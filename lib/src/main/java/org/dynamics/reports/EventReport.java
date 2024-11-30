3a742ad895b556f7b4ee082c92bdd7df890a9136
Vijayakumar1993 <vvijayakumarm@gmail.com>
2024-11-30 23:37:17 +0530

    init

--- lib/src/main/java/org/dynamics/reports/EventReport.java
+++ lib/src/main/java/org/dynamics/reports/EventReport.java
@@ -21,10 +21,15 @@ public class EventReport implements Report{
 
     private PdfWriter pdfWriter;
     private Document doc;
+    private Font NORMAL_FONT = new Font(Font.FontFamily.HELVETICA, 8, Font.NORMAL);
+    private Font H1 = new Font(Font.FontFamily.HELVETICA, 15, Font.BOLD);
+    private Font H2 = new Font(Font.FontFamily.HELVETICA, 12, Font.BOLD);
+    private Font H3 = new Font(Font.FontFamily.HELVETICA, 10, Font.BOLD);
+    private Font H4 = new Font(Font.FontFamily.HELVETICA, 8, Font.BOLD);
     public EventReport(String fileName) throws IOException, DocumentException {
         this.doc = new Document(PageSize.A4
                 ,3f,3f,3f,3f);
-        this.doc.setMargins(30, 30, 20, 20);
+        this.doc.setMargins(30, 30, 20, 50);
         this.pdfWriter = PdfWriter.getInstance(doc, Files.newOutputStream(Paths.get(fileName)));
         this.pdfWriter.setPageEvent(new PdfPageEventHelper(){
             @Override
@@ -32,7 +37,26 @@ public class EventReport implements Report{
                 ColumnText.showTextAligned(writer.getDirectContent(),
                         Element.ALIGN_CENTER,
                         new Phrase("Page " + writer.getPageNumber()),
-                        (doc.left() + doc.right()) / 2, doc.bottom() + 2, 0);
+                        (doc.left() + doc.right()) / 2, doc.bottom()-20 , 0);
+
+                //watermark
+                PdfContentByte canvas = writer.getDirectContentUnder();
+                Image watermarkImage = null; // Path to your image
+                try {
+                    watermarkImage = Image.getInstance("watermark.png");
+                } catch (BadElementException e) {
+                    throw new RuntimeException(e);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                watermarkImage.setAbsolutePosition(50, 100); // Position of the watermark image
+                watermarkImage.scaleToFit(400, 400); // Resize the image to fit
+                watermarkImage.setRotationDegrees(45); // Rotate image if needed
+                try {
+                    canvas.addImage(watermarkImage);
+                } catch (DocumentException e) {
+                    throw new RuntimeException(e);
+                }
             }
         });
         System.out.println("PDF Document created successfully...!");
@@ -42,23 +66,62 @@ public class EventReport implements Report{
         doc.open();
         if(event.getParentEvent()==null){
             List<Match> matches = event.getMatcher().getMatches();
-            Paragraph categoryName = new Paragraph(event.getEventName());
-            categoryName.setAlignment(Paragraph.ALIGN_CENTER);
-            doc.add(categoryName);
-            Paragraph teamName = new Paragraph(event.getEventName());
-            teamName.setAlignment(Paragraph.ALIGN_CENTER);
-            doc.add(teamName);
 
+            PdfPTable titleTable = new PdfPTable(3);
+            titleTable.setWidthPercentage(100);
+            Paragraph titleParagraph = new Paragraph();
+            try {
+                Image img = Image.getInstance("left-logo.jpeg");
+                img.scaleToFit(100, 100);
+                img.setAlignment(Image.ALIGN_LEFT); // Align image to center
+
+                PdfPCell imageCell = new PdfPCell();
+                imageCell.addElement(img);
+                imageCell.setBorder(0);
+                titleTable.addCell(imageCell);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+
+            Chunk categoryName = new Chunk(event.getEventName(),H1);
+            titleParagraph.add(categoryName);
+            titleParagraph.add("\n");
+            Chunk teamName = new Chunk(event.getEventName(),H2);
+            titleParagraph.add(teamName);
+            titleParagraph.add("\n");
+            Chunk drawSheet = new Chunk("Draw Sheet",H3);
+            titleParagraph.add(drawSheet);
+            titleParagraph.add("\n");
+            titleParagraph.add("\n");
+
+            titleParagraph.setAlignment(Paragraph.ALIGN_CENTER);
+
+
+            PdfPCell titleCell = new PdfPCell();
+            titleCell.addElement(titleParagraph);
+            titleCell.setBorder(0);
+            titleTable.addCell(titleCell);
+            try {
+                Image img1 = Image.getInstance("right-logo.png");
+                img1.scaleToFit(100, 100);
+                img1.setAlignment(Image.ALIGN_RIGHT); // Align image to center
+                PdfPCell rightCell = new PdfPCell();
+                rightCell.addElement(img1);
+                rightCell.setBorder(0);
+                titleTable.addCell(rightCell);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            doc.add(titleTable);
             doc.add(new LineSeparator());
 
-            Paragraph asOfDate = new Paragraph("As of "+ DateTimeFormatter.ofPattern("EEE dd MMM yyyy").format(LocalDate.now()));
-            asOfDate.setSpacingAfter(20);
+            Paragraph asOfDate = new Paragraph("As of "+ DateTimeFormatter.ofPattern("EEE dd MMM yyyy").format(LocalDate.now()),H3);
             asOfDate.setAlignment(Paragraph.ALIGN_CENTER);
             doc.add(asOfDate);
 
 
-
-            Paragraph noOfBoxers = new Paragraph("No of Boxers :"+noOFBoxers(event));
+            Paragraph noOfBoxers = new Paragraph("No of Boxers :"+noOFBoxers(event),H3);
             noOfBoxers.setAlignment(Paragraph.ALIGN_CENTER);
             doc.add(noOfBoxers);
             doc.add(new Paragraph("\n"));
@@ -97,18 +160,19 @@ public class EventReport implements Report{
                 }
                 options.add(innerList);
             });
-            Font font = new Font(Font.FontFamily.HELVETICA, 8, Font.NORMAL);
+
             options.forEach(a->{
                 a.stream().filter(Objects::nonNull).forEach(c->{
                     PdfPCell cell = new PdfPCell();
                     cell.setBorder(c.border);
-                    cell.addElement(new Phrase(c.message,font));
+                    cell.addElement(new Phrase(c.message,NORMAL_FONT));
                     table.addCell(cell);
                 });
             });
             doc.add(table);
 
         }
+
         doc.close();
         this.pdfWriter.close();
         JOptionPane.showMessageDialog(null, "PDF Generated successfully.");
